ecb2e99cc9cae45a66710b0dc7aaa4e21d546ea8
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-07-04 15:13:48 -0700

    Implement first section of videos on Media page, creating YouTubeEmbed component as well

--- src/data/news.js
+++
@@ -1,8 +0,0 @@
-export const newsEvents = [
-  {
-    title: "Helia Music Collective is excited to announce their first concert, featuring Siroko Duo!",
-    image:
-      url: "https://docs.astro.build/assets/rose.webp"
-      alt: "The Astro logo on a dark background with a pink glow."
-  }
-]
