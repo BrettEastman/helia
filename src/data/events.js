480711a71c601558e3a951e5513a9a98d9c75182
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-07-04 09:47:38 -0700

    Remove markdown blog posts and replace with CardEvent and events.js data object, formatting CSS to match their current site

---
+++ src/data/events.js
@@ -0,0 +1,33 @@
+const events = [
+  {
+    title: "Vernon Salon Series",
+    concertDate: "February 17, 2017, 8pm",
+    venue: "Vernon Salon",
+    venueAddress: "50 Oak Street",
+    city: "Oakland",
+    tickets: "Tickets: $20 general, $15 emerging artist/BYOB discount",
+    description:
+      "For the second Vernon Salon of 2017, flutist Jessie Nucho will be performing two solos works by Kaija Saariaho and Eve Beglarian, followed with talks by composer Emma Logan, mixed media artist Victoria Welling, and writer Lindsay Merbaum on women in the art world.",
+  },
+  {
+    title: "Re:Ignite - Helia Music Collective & Siroko Duo",
+    concertDate: "March 25, 2017, 7:30pm",
+    venue: "Center for New Music",
+    venueAddress: "55 Taylor Street",
+    city: "San Francisco",
+    tickets: "Tickets: $15 general, $10 members/students/seniors",
+    description:
+      "Helia Music Collective and Siroko Duo present a program of contemporary works by women composers for two flutes. The concert will feature the world premiere of works by Chelsea Loew and Emma Logan, as well as recent works by Julie Barwick, Shulamit Ran, Izabel Austin, Jane Rigler, and Brandy Hudelson.",
+  },
+  {
+    title: "Return: ​Helia & SFCM Clarinet Ensemble",
+    concertDate: "Wednesday, March 23, 2022, 7:30pm",
+    venue: "San Francisco Conservatory of Music – Concert Hall",
+    venueAddress: "50 Oak Street",
+    city: "San Francisco",
+    description:
+      "Helia combines forces with Jeff Anderle and the SFCM Clarinet Ensemble to premiere four newly-commissioned large clarinet ensemble works by composers Theresa Wong, Shawne Workman, Emma Logan, and Julie Barwick.",
+  },
+];
+
+export default events;
