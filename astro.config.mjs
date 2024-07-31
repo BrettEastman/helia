8013dffd1b398e838514b18867d644ef287a5edb
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-07-30 23:20:49 -0700

    Reinstall astro-robots-txt as a devDependency, modify astro.config.mjs to include policy, and do astro build to verify robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), robotsTxt()]
+  site: "https://www.heliamusiccollective.com/",
+  integrations: [
+    icon(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+          crawlDelay: 10,
+        },
+      ],
+    }),
+  ],
 });
