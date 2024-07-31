de1eddb46353082d1122658738f75ffc4122081f
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-07-30 20:16:02 -0700

    Add astro-robots-txt dependency to create robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), robotsTxt()]
 });
